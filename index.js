91ecd8d227976db6e4b28e216b3d51b819428da4
Nick Thompson <ncthom91@gmail.com>
2013-01-23 23:57:42 -0500

    Rewritten to uphold spec.

--- index.js
+++ index.js
@@ -3,32 +3,22 @@
  * Simple delay implementation for the Web Audio API.
  *
  * @param {AudioContext} context
- * @param {Int} type 0: normal, 1: inverted, 2: ping-pong
- * @param {Float} delay Delay time in seconds
- * @param {Float} feedback Delay feedback level
+ * @param {number} type
+ * @param {number} delay
+ * @param {number} feedback
  */
 
 function Delay (context, type, delay, feedback) {
   this.input = context.createGainNode();
   this.output = context.createGainNode();
 
-  // Default values
-  type = type         || 0;
-  delay = delay       || 1.0;
-  feedback = feedback || 0.5;
-
+  // Internal AudioNodes
   this._split = context.createChannelSplitter(2);
   this._merge = context.createChannelMerger(2);
   this._leftDelay = context.createDelayNode();
   this._rightDelay = context.createDelayNode();
   this._leftGain = context.createGainNode();
   this._rightGain = context.createGainNode();
-
-  // Expose AudioParams nicely for clean automation and value assignment
-  this.delayLeft = this._leftDelay.delayTime;
-  this.delayRight = this._rightDelay.delayTime;
-  this.feedbackLeft = this._leftGain.gain;
-  this.feedbackRight = this._rightGain.gain;
 
   // AudioNode graph routing
   this.input.connect(this._split);
@@ -37,52 +27,117 @@ function Delay (context, type, delay, feedback) {
   this._leftGain.connect(this._merge, 0, 0);
   this._rightGain.connect(this._merge, 0, 1);
   this._merge.connect(this.output);
+  this[["_routeNormal", "_routeInverted", "_routePingPong"][type || 0]]();
 
-  this[["_routeNormal", "_routeInverted", "_routePingPong"][type]]();
-
-  // Value assignment
-  this.delayRight.setValueAtTime(delay, 0);
-  this.delayLeft.setValueAtTime(delay, 0);
-  this.feedbackRight.setValueAtTime(feedback, 0);
-  this.feedbackLeft.setValueAtTime(feedback, 0);
+  // Defaults
+  for (var prop in this.meta) {
+    this[prop] = this.meta[prop].defaultValue;
+  }
 }
 
-/**
- * Various routing schemes.
- */
+Delay.prototype = Object.create(null, {
 
-Delay.prototype._routeNormal = function () {
-  this._split.connect(this._leftDelay, 0);
-  this._split.connect(this._rightDelay, 1);
-  this._leftGain.connect(this._leftDelay);
-  this._rightGain.connect(this._rightDelay);
-};
+  /**
+   * AudioNode prototype `connect` method.
+   *
+   * @param {AudioNode} dest
+   */
 
-Delay.prototype._routeInverted = function () {
-  this._split.connect(this._leftDelay, 1);
-  this._split.connect(this._rightDelay, 0);
-  this._leftGain.connect(this._leftDelay);
-  this._rightGain.connect(this._rightDelay);
-};
+  connect: {
+    value: function (dest) {
+      this.output.connect(dest);
+    }
+  },
 
-Delay.prototype._routePingPong = function () {
-  this._split.connect(this._leftDelay, 0);
-  this._split.connect(this._rightDelay, 1);
-  this._leftGain.connect(this._rightDelay);
-  this._rightGain.connect(this._leftDelay);
-}
+  /**
+   * AudioNode prototype `disconnect` method.
+   */
 
-/**
- * Mimick AudioNode prototype functionality with `connect` and `disconnect`.
- */
+  disconnect: {
+    value: function () {
+      this.output.disconnect();
+    }
+  },
 
-Delay.prototype.connect = function (dest) {
-  this.output.connect(dest);
-};
+  /**
+   * Module parameter metadata.
+   */
 
-Delay.prototype.disconnect = function () {
-  this.output.disconnect();
-};
+  meta: {
+    value: {
+      delay: {
+        min: 0,
+        max: 10,
+        defaultValue: 1.0,
+        type: "pot"
+      },
+      feedback: {
+        min: 0,
+        max: 1,
+        defaultValue: 0.5,
+        type: "pot"
+      }
+    }
+  },
+
+  /**
+   * Various routing schemes.
+   */
+
+  _routeNormal: {
+    value: function () {
+      this._split.connect(this._leftDelay, 0);
+      this._split.connect(this._rightDelay, 1);
+      this._leftGain.connect(this._leftDelay);
+      this._rightGain.connect(this._rightDelay);
+    }
+  },
+
+  _routeInverted: {
+    value: function () {
+      this._split.connect(this._leftDelay, 1);
+      this._split.connect(this._rightDelay, 0);
+      this._leftGain.connect(this._leftDelay);
+      this._rightGain.connect(this._rightDelay);
+    }
+  },
+
+  _routePingPong: {
+    value: function () {
+      this._split.connect(this._leftDelay, 0);
+      this._split.connect(this._rightDelay, 1);
+      this._leftGain.connect(this._rightDelay);
+      this._rightGain.connect(this._leftDelay);
+    }
+  },
+
+  /**
+   * Public delay parameter.
+   */
+
+  delay: {
+    enumerable: true,
+    get: function () { return this._leftDelay.delayTime.value; },
+    set: function (value) {
+      this._leftDelay.delayTime.setValueAtTime(value, 0);
+      this._rightDelay.delayTime.setValueAtTime(value, 0);
+    }
+  },
+
+  /**
+   * Public feedback parameter.
+   */
+
+  feedback: {
+    enumerable: true,
+    get: function () { return this._leftGain.gain.value; },
+    set: function (value) {
+      this._leftGain.gain.setValueAtTime(value, 0);
+      this._rightGain.gain.setValueAtTime(value, 0);
+    }
+  }
+
+});
 
 /**
  * Exports.
